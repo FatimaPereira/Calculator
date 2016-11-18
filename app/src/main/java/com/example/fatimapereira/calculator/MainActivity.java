3e18e2b6b3710def91872a8dcdd1f894162f3df9
Fatima Pereira <pfati.34@gmail.com>
2016-11-18 11:29:32 +0100

    UPDATE btnAction
    Method displayNumber

--- app/src/main/java/com/example/fatimapereira/calculator/MainActivity.java
+++ app/src/main/java/com/example/fatimapereira/calculator/MainActivity.java
@@ -9,6 +9,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNumber0;
+    Button btnNumber1;
+    Button btnNumber2;
+    Button btnNumber3;
+    Button btnNumber4;
+    Button btnNumber5;
+    Button btnNumber6;
+    Button btnNumber7;
+    Button btnNumber8;
+    Button btnNumber9;
+    Button btnDecimalSeparator;
+
+    Button btnDivision;
+    Button btnMultiplication;
+    Button btnSubtraction;
+    Button btnAddition;
+    Button btnResultOperator;
+
+    Button btnReset;
+    TextView textViewField;
+
+    boolean update = false;
+    String saveText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,121 +43,120 @@ public class MainActivity extends AppCompatActivity {
         //*******************************************************
         //        NUMBER
         //*******************************************************
-        Button btnNumber0 = (Button) findViewById(R.id.btn_0);
-        Button btnNumber1 = (Button) findViewById(R.id.btn_1);
-        Button btnNumber2 = (Button) findViewById(R.id.btn_2);
-        Button btnNumber3 = (Button) findViewById(R.id.btn_3);
-        Button btnNumber4 = (Button) findViewById(R.id.btn_4);
-        Button btnNumber5 = (Button) findViewById(R.id.btn_5);
-        Button btnNumber6 = (Button) findViewById(R.id.btn_6);
-        Button btnNumber7 = (Button) findViewById(R.id.btn_7);
-        Button btnNumber8 = (Button) findViewById(R.id.btn_8);
-        Button btnNumber9 = (Button) findViewById(R.id.btn_9);
+        btnNumber0 = (Button) findViewById(R.id.btn_0);
+        btnNumber1 = (Button) findViewById(R.id.btn_1);
+        btnNumber2 = (Button) findViewById(R.id.btn_2);
+        btnNumber3 = (Button) findViewById(R.id.btn_3);
+        btnNumber4 = (Button) findViewById(R.id.btn_4);
+        btnNumber5 = (Button) findViewById(R.id.btn_5);
+        btnNumber6 = (Button) findViewById(R.id.btn_6);
+        btnNumber7 = (Button) findViewById(R.id.btn_7);
+        btnNumber8 = (Button) findViewById(R.id.btn_8);
+        btnNumber9 = (Button) findViewById(R.id.btn_9);
 
-        Button btnDecimalSeparator = (Button) findViewById(R.id.btn_decimal_separator);
-
-
+        btnDecimalSeparator = (Button) findViewById(R.id.btn_decimal_separator);
 
 
         //--- NUMBER OnClick --------------------------------
         btnNumber0.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 0;
+            int number = 0;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
+
+
         });
 
         btnNumber1.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 1;
+            int number = 1;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber2.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 2;
+            int number = 2;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber3.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 3;
+            int number = 3;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber4.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 4;
+            int number = 4;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber5.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 5;
+            int number = 5;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber6.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 6;
+            int number = 6;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber7.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 7;
+            int number = 7;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber8.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 8;
+            int number = 8;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnNumber9.setOnClickListener(new View.OnClickListener() {
-            int btnNumber = 9;
+            int number = 9;
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
 
         btnDecimalSeparator.setOnClickListener(new View.OnClickListener() {
-            String decimalSeparateur = ",";
+            String number = ".";
             @Override
             public void onClick(View view) {
-
+                displayNumber(number);
             }
         });
-
 
 
         //*******************************************************
         //        OPERATOR
         //*******************************************************
-        Button btnDivision = (Button) findViewById(R.id.btn_division);
-        Button btnMultiplication = (Button) findViewById(R.id.btn_multiplication);
-        Button btnSubtraction = (Button) findViewById(R.id.btn_subtraction);
-        Button btnAddition = (Button) findViewById(R.id.btn_addition);
-        Button btnResultOperator = (Button) findViewById(R.id.btn_result);
+        btnDivision = (Button) findViewById(R.id.btn_division);
+        btnMultiplication = (Button) findViewById(R.id.btn_multiplication);
+        btnSubtraction = (Button) findViewById(R.id.btn_subtraction);
+        btnAddition = (Button) findViewById(R.id.btn_addition);
+        btnResultOperator = (Button) findViewById(R.id.btn_result);
 
         //--- OPERATOR OnClick --------------------------------
         btnDivision.setOnClickListener(new View.OnClickListener() {
@@ -183,35 +206,45 @@ public class MainActivity extends AppCompatActivity {
         //        OTHER
         //*******************************************************
 
-        Button btnReset = (Button) findViewById(R.id.btn_c);
-        TextView textViewField = (TextView) findViewById(R.id.field_number);
-        textViewField.getText();
+        btnReset = (Button) findViewById(R.id.btn_c);
+        textViewField = (TextView) findViewById(R.id.field_number);
+        saveText = textViewField.getText().toString();
 
         //--- OTHER OnClick --------------------------------
 
-       //textViewField.setText("0");
+        //textViewField.setText("0");
 
         btnReset.setOnClickListener(new View.OnClickListener() {
-            int btnReset = 0;
             @Override
             public void onClick(View view) {
-
-
+                    textViewField.setText("0");
             }
         });
-
+    }
         //--- METHOD Number --------------------------------
-        // textViewField.setText(displayResult(textViewField.getText().toString()));
-//        int number;
-//        int btnNumber;
-//        String displayResult(String number) {
-//               if (btnNumber == 0) {
-//                 number = btnNumber;
-//               } else {
-//                  number = number + btnNumber;
-//               }
-//        }
 
+        //Affichage du chiffre à l'écran
+
+    public void displayNumber(String string) {
+        saveText = textViewField.getText().toString();
+        if (saveText.equals("0")) {
+            textViewField.setText(string);
+        } else {
+            textViewField.setText(saveText + string);
+        }
+
+    }
+    // (String.valueOf(btnNumber)) =
+    public void displayNumber(int btnNumber) {
+        saveText = textViewField.getText().toString();
+        if (saveText.equals("0")) {
+            textViewField.setText(String.valueOf(btnNumber));
+        } else {
+            textViewField.setText(saveText + btnNumber);
+        }
 
     }
 }
+
+
+
